0b4ffb1c1a42fe014ba12a1759f417f51dc36990
wachira <neticcul2se@users.noreply.github.com>
2020-04-21 21:03:30 +0700

    firt update flutter

---
+++ flutterfruits/android/app/src/main/kotlin/com/example/flutterfruits/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterfruits
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ onlineshop/android/app/src/main/kotlin/com/wachira/onlineshop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.wachira.onlineshop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
